322d83bfc4e4755c06e6f3037b96f1b0dee86806
Noodle <nerddy.hahaha@hotmail.com>
2018-08-28 16:08:22 +0700

    UI_Page_History Readme.md

--- BorrowV/src/app/app.component.ts
+++ BorrowV/src/app/app.component.ts
@@ -5,12 +5,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
+import { CreateitemPage } from '../pages/createitem/createitem';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = CreateitemPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
---
+++ BorrowV/src/pages/viewdetail/viewdetail.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ViewdetailPage } from './viewdetail';
+
+@NgModule({
+  declarations: [
+    ViewdetailPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ViewdetailPage),
+  ],
+})
+export class ViewdetailPageModule {}
