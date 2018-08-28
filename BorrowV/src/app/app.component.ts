1aa93c0f6efb475eccc49c2bbb827dbebe34a2be
thaninpong <thaninpong@perfenterprise.com>
2018-08-28 17:13:18 +0700

    edit page

--- BorrowV/src/app/app.component.ts
+++ BorrowV/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginPage } from '../pages/login/login';
-import { HistoryPage } from '../pages/History/history';
-import { DetailhistoryPage } from '../pages/detailhistory/detailhistory';
 
 
 @Component({
