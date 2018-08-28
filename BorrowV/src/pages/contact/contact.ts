d54fd4200db9b762b0bddf4f20dd7a7167b5cdc8
thaninpong <thaninpong@perfenterprise.com>
2018-08-28 14:21:41 +0700

    Create apge login,createitem

--- BorrowV/src/pages/contact/contact.ts
+++ BorrowV/src/pages/contact/contact.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { CreateitemPage } from '../createitem/createitem';
 
 @Component({
   selector: 'page-contact',
@@ -10,5 +12,10 @@ export class ContactPage {
   constructor(public navCtrl: NavController) {
 
   }
-
+Go(){
+  this.navCtrl.push(LoginPage);
+}
+Createitem(){
+  this.navCtrl.push(CreateitemPage);
+}
 }
