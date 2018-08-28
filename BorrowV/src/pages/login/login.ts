9cd797c51e259545822e85bc474b7da3cd751d03
thaninpong <thaninpong@perfenterprise.com>
2018-08-28 16:49:32 +0700

    edit bug

--- BorrowV/src/pages/login/login.ts
+++ BorrowV/src/pages/login/login.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { TabsPage } from '../tabs/tabs';
+import { IonicPage, NavController, NavParams } from 'ionic-angular'; 
 import { ContactPage } from '../contact/contact';
 
 /**
